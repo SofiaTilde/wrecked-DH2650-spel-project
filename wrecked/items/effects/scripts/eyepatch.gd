9422b1ae4b1cf306b31a04af91b55a062f963567
Sofia Andersson <sofiaa5@kth.se>
2025-04-21 19:05:39 +0200

    Restructure item code

--- wrecked/items/effects/scripts/eyepatch.gd
+++ wrecked/items/effects/scripts/eyepatch.gd
@@ -1,7 +1,5 @@
 extends Item
 
-var textureNode: TextureRect
-
 func _ready():
 	labelText = "Eyepatch"
 	overlayTexture = preload("res://items/effects/textures/eyepatch.png") as Texture2D
@@ -10,18 +8,4 @@ func activateItem():
 	var player = detectHitPlayer()
 	if player == null:
 		return
-	await applyEffect(player)
-
-func applyEffect(player: CharacterBody3D):
-	textureNode = player.get_node("ItemEffect/TextureRect")
-	textureNode.texture = overlayTexture
-
-	var timerNode = Timer.new()
-	add_child(timerNode)
-	timerNode.wait_time = 3.0
-	timerNode.one_shot = true
-	timerNode.start()
-	await timerNode.timeout
-
-	#remove effect
-	textureNode.texture = null
+	await applyOverlay(player, 3.0)
--- wrecked/items/item_class.gd
+++ wrecked/items/item_class.gd
@@ -19,3 +19,19 @@ func detectHitPlayer() -> CharacterBody3D:
 	#have an detect hit that returns hit player
 	#temp always player 1
 	return get_tree().root.get_node("Level/GridContainer/SubViewportContainer/SubViewport/Player") as CharacterBody3D
+
+func timer(seconds):
+	var timerNode = Timer.new()
+	add_child(timerNode)
+	timerNode.wait_time = seconds
+	timerNode.one_shot = true
+	timerNode.start()
+	await timerNode.timeout
+
+func applyOverlay(player: CharacterBody3D, seconds):
+	var textureNode = player.get_node("ItemEffect/TextureRect") as TextureRect
+	textureNode.texture = overlayTexture
+	#leave effect for a while
+	await timer(seconds)
+	#remove effect
+	textureNode.texture = null
