eb2962e0e9124d04897ab9bc50f32bf809858aab
Sofia Andersson <sofiaa5@kth.se>
2025-05-09 20:46:45 +0200

    Increase barrel height

--- wrecked/items/barrel/barrel.gd
+++ wrecked/items/barrel/barrel.gd
@@ -34,7 +34,7 @@ func _on_body_entered(body) -> void:
 func barrelStart(x, z, level):
 	barrel_level = level
 	setTexture()
-	self.global_position = Vector3(x, 1.375, z)
+	self.global_position = Vector3(x, 1.5, z)
 	self.visible = true
 
 func setTexture():
@@ -48,3 +48,21 @@ func setTexture():
 	else: # if not gold/silver, set as copper
 		barrel_level = BarrelLevel.Copper
 		mesh.set_surface_override_material(0, material_copper)
+
+func _process(delta: float) -> void:
+	var pos = global_transform.origin
+
+	pos.y = 0.0
+
+	var time = Time.get_ticks_msec() / 1000.0
+
+	pos.y += sin(pos.x * 0.51 + pos.z * 0.56 + time * 0.6) * 0.41;
+	pos.y += sin(pos.x * 0.73 + pos.z * 0.392 + time * 0.9) * 0.35;
+	pos.y += sin(pos.x * 0.398 + pos.z * 0.2 + time * 0.7) * 0.43;
+	pos.y += sin(pos.x * 0.484 + pos.z * 0.82 + time * 0.82) * 0.21;
+	pos.y += sin(pos.x * 0.212 + pos.z * 0.47 + time * 0.75) * 0.32;
+
+	pos.y *= pow(clamp(1.0 - sqrt(pos.x * pos.x + pos.z * pos.z) / 128.0, 0.05, 1.0), 0.5);
+	pos.y += 0.25 + 1.5 + 0.25
+
+	global_transform.origin = pos
