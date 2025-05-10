1e3a06f5098fdaa69fe30aeab403f2dfba7c3cc8
Sofia Andersson <sofiaa5@kth.se>
2025-05-10 20:47:54 +0200

    Screen change simple animation

--- wrecked/items/effects/scripts/screen_change.gd
+++ wrecked/items/effects/scripts/screen_change.gd
@@ -12,20 +12,23 @@ extends Item
 func _ready():
 	labelText = "None"
 	icon = null
+
 	var player1 = get_tree().root.get_node("Game/GridContainer/SubViewportContainer/SubViewport/Player") as CharacterBody3D
 	var player2 = get_tree().root.get_node("Game/GridContainer/SubViewportContainer2/SubViewport/Player2") as CharacterBody3D
 	var player3 = get_tree().root.get_node("Game/GridContainer/SubViewportContainer3/SubViewport/Player3") as CharacterBody3D
 	var player4 = get_tree().root.get_node("Game/GridContainer/SubViewportContainer4/SubViewport/Player4") as CharacterBody3D
+
 	view1 = player1.viewPortTexture
 	view2 = player2.viewPortTexture
 	view3 = player3.viewPortTexture
 	view4 = player4.viewPortTexture
 
+	shaderMaterial = preload("res://items/effects/materials/screen_change.tres") as ShaderMaterial
 
 func activateItem():
-	changeScreen()
-	await timer(4)
-	resetScreen()
+	await changeScreen()
+	await timer(10)
+	await resetScreen()
 
 func changeScreen():
 	text1.visible = true
@@ -33,18 +36,42 @@ func changeScreen():
 	text3.visible = true
 	text4.visible = true
 
+	text1.material = shaderMaterial
+	text2.material = shaderMaterial
+	text3.material = shaderMaterial
+	text4.material = shaderMaterial
+
+	await timer(0.5)
 	text1.texture = view4
 	text2.texture = view3
 	text3.texture = view2
 	text4.texture = view1
+	await timer(0.5)
+
+	text1.material = null
+	text2.material = null
+	text3.material = null
+	text4.material = null
 
 func resetScreen():
-	text1.visible = false
-	text2.visible = false
-	text3.visible = false
-	text4.visible = false
+	text1.material = shaderMaterial
+	text2.material = shaderMaterial
+	text3.material = shaderMaterial
+	text4.material = shaderMaterial
 
+	await timer(0.5)
 	text1.texture = view1
 	text2.texture = view2
 	text3.texture = view3
 	text4.texture = view4
+	await timer(0.5)
+
+	text1.material = null
+	text2.material = null
+	text3.material = null
+	text4.material = null
+
+	text1.visible = false
+	text2.visible = false
+	text3.visible = false
+	text4.visible = false
