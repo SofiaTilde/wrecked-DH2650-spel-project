ff51ad04dbdf7281c928d20363fe6716caab5574
Sofia Andersson <sofiaa5@kth.se>
2025-05-10 21:09:20 +0200

    Fix screen change transfer

--- wrecked/items/generators/items.gd
+++ wrecked/items/generators/items.gd
@@ -30,15 +30,15 @@ func get_item(level):
 
 func get_copper_item():
 	if copperItemlist.is_empty():
-		copperItemlist = [screen_change]#bottle_of_rum, shroom, eyepatch, anchor]
+		copperItemlist = [screen_change, bottle_of_rum, shroom, eyepatch, anchor]
 	return copperItemlist.pick_random()
 
 func get_silver_item():
 	if silverItemlist.is_empty():
-		silverItemlist = [screen_change]#bottle_of_rum, shroom, eyepatch, anchor]
+		silverItemlist = [screen_change, bottle_of_rum, shroom, eyepatch, anchor]
 	return silverItemlist.pick_random()
 
 func get_gold_item():
 	if goldItemlist.is_empty():
-		goldItemlist = [screen_change]#bottle_of_rum, shroom, eyepatch, anchor]
+		goldItemlist = [screen_change, bottle_of_rum, shroom, eyepatch, anchor]
 	return goldItemlist.pick_random()
