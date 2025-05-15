84f11723589b0395f8ff005aaea75e84c1c9b28e
Sofia Andersson <sofiaa5@kth.se>
2025-05-16 00:59:10 +0200

    Add molotov shader

--- wrecked/items/effects/scripts/molotov_cocktail.gd
+++ wrecked/items/effects/scripts/molotov_cocktail.gd
@@ -3,8 +3,10 @@ extends Item
 func _ready():
 	labelText = "Molotov Cocktail"
 	icon = preload("res://items/molotov_cocktail/molotov_cocktail_icon.png") as Texture2D
+	shaderMaterial = preload("res://items/effects/materials/molotov_cocktail.tres") as ShaderMaterial
 
 func activateItem():
 	var player = detectHitPlayer()
 	if player == null:
 		return
+	await applyShader(player, 7)
