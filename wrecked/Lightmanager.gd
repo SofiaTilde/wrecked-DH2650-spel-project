467ef5269214454a612c364cfdc73f6aaf86a72c
AKSB-GP <alexander.kazakov1999@gmail.com>
2025-05-19 13:38:42 +0200

    outline working

--- wrecked/Lightmanager.gd
+++ wrecked/Lightmanager.gd
@@ -1,6 +1,6 @@
 extends DirectionalLight3D
 
-@onready var GlobaLlight = $root/game/DirectionalLight3D
+@onready var GlobaLlight = $"."
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,8 +8,9 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+#används för att få tillgång till globala ljus riktningen för shading exempelvis
 func _process(delta: float) -> void:
 	if GlobaLlight:
 		#is this needed?
-		var light_dir = -GlobaLlight.global_transform.basis.z.normalized()
+		var light_dir = -GlobaLlight.global_transform.basis.z
 		RenderingServer.global_shader_parameter_set("global_light_dir", light_dir)
