3c74b22a766ae11c6cbebb72e570dd448e3ad058
Sofia Andersson <sofiaa5@kth.se>
2025-05-23 15:20:52 +0200

    Add dynamite effect

--- wrecked/items/effects/scripts/dynamite.gd
+++ wrecked/items/effects/scripts/dynamite.gd
@@ -5,10 +5,18 @@ var activePlayer: CharacterBody3D
 var velocityX: float
 var velocityY: float
 var velocityZ: float
+var frame1: Texture2D
+var frame2: Texture2D
+var frame3: Texture2D
+var frame4: Texture2D
 
 func _ready():
 	labelText = "Dynamite"
 	icon = preload("res://items/dynamite/dynamite_icon.png") as Texture2D
+	frame1 = preload("res://items/effects/textures/explosion/1.png") as Texture2D
+	frame2 = preload("res://items/effects/textures/explosion/2.png") as Texture2D
+	frame3 = preload("res://items/effects/textures/explosion/3.png") as Texture2D
+	frame4 = preload("res://items/effects/textures/explosion/4.png") as Texture2D
 
 func activateItem():
 	activePlayer = detectHitPlayer()
@@ -20,7 +28,15 @@ func activateItem():
 	activePlayer.velocity = Vector3(0, 0, 0)
 	active = true
 	velocityY = 3
-	await timer(0.2)
+	overlayTexture = frame1
+	await applyOverlayNoNull(activePlayer, 0.05)
+	overlayTexture = frame2
+	await applyOverlayNoNull(activePlayer, 0.05)
+	overlayTexture = frame3
+	await applyOverlay(activePlayer, 0.05)
+	overlayTexture = frame4
+	await applyOverlay(activePlayer, 0.05)
+	#await timer(0.2)
 	velocityY = 0
 	velocityX = rng.randf_range(-1, 1) * 5
 	velocityZ = rng.randf_range(-1, 1) * 5
